63adc8f73b6140aae8594e5a1dcd8b43909e7ffd
Martin Kemp <martinkemp@deloitte.co.uk>
2019-05-15 17:18:36 +0100

    Buildscript

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ pipenv install --deploy
 SITE_PACKAGES=$(pipenv --venv)/lib/python3.7/site-packages
 echo "Library Location: $SITE_PACKAGES"
 cd ${SITE_PACKAGES}
-7z a -tzip -mx=9 ${DIR}/package.zip * -xr!boto3 -x!boto3* -xr!botocore -x!botocore*
+7z a -tzip -mx=9 ${DIR}/package.zip * -x!boto3 -x!boto3* -x!botocore -x!botocore*
 7z a -tzip -mx=9 ${DIR}/package.zip boto3_type_annotations*
 cd ${DIR}
-7z d package.zip pip pip* setuptools setuptools* wheel wheel* pkg_resources pkg_resources* easy_install easy_install* *.dist-info -r
+7z d package.zip pip pip* setuptools setuptools* wheel wheel* pkg_resources pkg_resources* easy_install easy_install* -r
 7z a package.zip router.py config.ini
